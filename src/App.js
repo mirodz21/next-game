8476755652eb48eb23b7e6598b104fb1aa07fd12
unknown <mi.rodz21@gmail.com>
2022-11-09 13:12:51 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from "./components/directory/directory.component";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Consoles",
+      imageUrl: "https://i.ibb.co/vvW535g/Png-Item-65683.png",
+    },
+    {
+      id: 2,
+      title: "Handhelds",
+      imageUrl: "https://i.ibb.co/XxmjHK0/Png-Item-4685454.png",
+    },
+    {
+      id: 3,
+      title: "Games",
+      imageUrl: "https://i.ibb.co/Lr7Z3Q5/Video-Games-612x445-1.jpg",
+    },
+    {
+      id: 4,
+      title: "Accessories",
+      imageUrl: "https://i.ibb.co/rf8v41d/Png-Item-1234395.png",
+    },
+    {
+      id: 5,
+      title: "Parts and Replacements",
+      imageUrl:
+        "https://i.ibb.co/crYVMbW/Hc72296ee88294753b700aaee30920d7b-V.jpg",
+    },
+  ];
+
+  return <Directory categories={categories} />;
+};
 
 export default App;
